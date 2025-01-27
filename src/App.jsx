74d1838c56940b50da31c8e056ec9b9bffcfb3ee
Edita Adomaityte <adomaityteedita02@gmail.com>
2025-01-27 11:09:18 -0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Route, Routes } from "react-router-dom"
+import "./App.css"
+import { Authorized } from "./views/Authorized"
+import { ApplicationViews } from "./views/ApplicationViews"
+
+
+export const App=()=>{
+    return(
+        <Routes>
+            <Route path="/login" element={<Login/>}/>
+            <Route path="/register" element={<Register/>}/>
+
+            <Route path="/*" element={
+                <Authorized>
+                    <ApplicationViews />
+                </Authorized>
+            }/>
+        </Routes>
+    )
+}
