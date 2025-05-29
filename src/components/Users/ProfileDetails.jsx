4c4a4947f91c79d90e449334d5f85bf62322a471
Edita Adomaityte <adomaityteedita02@gmail.com>
2025-05-29 10:30:46 -0500

    Bulma implementation Organizers List

--- src/components/Users/ProfileDetails.jsx
+++ src/components/Users/ProfileDetails.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import "./Users.css";
+
 import { useNavigate, useParams } from "react-router-dom";
 import { getUserById } from "../../services/userService";
 
@@ -17,36 +17,38 @@ export const ProfileDetails = ({ currentUser }) => {
   }, [userId]);
 
   return (
-    <section className="user">
-      <h2>{user.name}</h2>
-      <div className="user-info2">
-        <span className="user-info1">City:</span>
-        {user.city}
-      </div>
-      <div className="user-info2">
-        <span className="user-info1">State:</span>
-        {user.state?.state_name}
-      </div>
-      <div className="user-info2">
-        <span className="user-info1">Email:</span>
-        {user.email}
-      </div>
-      <div className="user-info2">
-        <span className="user-info1"># of Events:</span>
-        {user.events?.length}
-      </div>
-      <div>
+    <section className="section">
+      <div className="box">
+        <h2 className="title is-4">{user.name}</h2>
+  
+        <div className="mb-2">
+          <span className="has-text-weight-semibold">City:</span> {user.city}
+        </div>
+  
+        <div className="mb-2">
+          <span className="has-text-weight-semibold">State:</span> {user.state?.state_name}
+        </div>
+  
+        <div className="mb-2">
+          <span className="has-text-weight-semibold">Email:</span> {user.email}
+        </div>
+  
+        <div className="mb-4">
+          <span className="has-text-weight-semibold"># of Events:</span> {user.events?.length}
+        </div>
+  
         {user.id === currentUser.id && (
-          <button
-            className="btn btn-edit"
-            onClick={() => {
-              navigate(`/profile/${user.id}/edit`);
-            }}
-          >
-            Edit
-          </button>
+          <div>
+            <button
+              className="button is-info is-small"
+              onClick={() => navigate(`/profile/${user.id}/edit`)}
+            >
+              Edit
+            </button>
+          </div>
         )}
       </div>
     </section>
   );
+  
 };
